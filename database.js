c7d34700d638ad9ab7ee7ddf898c33bfc2fa8f41
manasa-9168 <163173068+manasa-9168@users.noreply.github.com>
2024-07-10 06:33:03 +0530

    Create database.js

---
+++ database.js
@@ -0,0 +1,31 @@
+const {MongoClient}=require('mongodb');
+const url='mongodb://127.0.0.1:27017';
+const client=new MongoClient(url);
+let collection;
+
+async function connectDB(dbname,collectionname)
+{
+    let result=await client.connect();
+    let db=result.db(dbname);
+    collection=db.collection(collectionname);
+    console.log("DataBase Connected...");
+    return collection;
+}
+
+exports.getData=async function(name,password)
+{
+    collection=await connectDB("cseb","employee");
+    let response=await collection.find({name:name,password:password}).toArray();
+    console.log("From getData method: "+JSON.stringify(response));
+    collection.close;
+    return JSON.stringify(response);
+}
+
+exports.insertData=async function(emp)
+{
+    collection=await connectDB("cseb","employee");
+    let response=await collection.insertOne({name:emp.name,work:emp.work,password:emp.password})
+    console.log("Record Inserted Successfully");
+    collection.close;
+    return JSON.stringify(response);
+}
